3e98155b3c3c6d94348dd40206ff4e4e3b73b661
ce3003 <beaky98tkk@gmail.com>
2022-02-23 14:08:23 +0800

    Update Lab3

--- Lab3/ce3103/te2/work/part10/hello-funct2.c
+++ Lab3/ce3103/te2/work/part10/hello-funct2.c
@@ -1,7 +1,9 @@
 #include "hello.h"
+#include <stdio.h>
 
 int incrementcount(int count)
-{
-    delay(0x7fffffff);
-    return count + 1;
+{	
+	printf("Hello world from funct2!\n");    
+	delay(0x7fffffff);
+	return count + 1;
 }
--- Lab3/ce3103/te2/work/part10/hello-main.c
+++ Lab3/ce3103/te2/work/part10/hello-main.c
@@ -15,13 +15,8 @@ int main()
     int count = 0;
     while(count < 1)
     {
-        printf("press enter");
-        if(getchar())
-        {
             helloprint();
             count = incrementcount(count);
-            printf("current count: %d\n", count);
-        }
     }
 
     printf("Bye!\n");
